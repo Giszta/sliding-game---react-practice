8fa1a12ba3172a0b40689312e7167b2e7d3562cf
AdamGiszter <a.m.giszter@gmail.com>
2023-11-21 11:07:09 +0100

    Tile movement added

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -3,6 +3,11 @@ import Tile from "../tile/Tile";
 import "./Board.css";
 import { useState } from "react";
 
+export interface tileData {
+	value: number;
+	index: number;
+}
+
 export const Board = () => {
 	const shuffle = () =>
 		new Array(16)
@@ -11,15 +16,28 @@ export const Board = () => {
 			.sort(() => Math.random() - 0.5)
 			.map((x, i) => ({ value: x, index: i }));
 
-	const [numbers, SetNumbers] = useState(shuffle());
+	const [numbers, setNumbers] = useState(shuffle());
+
+	const moveTile = (tile: tileData) => {
+		const i16 = numbers.find((n) => n.value === 16)!.index;
+		if (![i16 - 1, i16 + 1, i16 - 4, i16 + 4].includes(tile.index)) return;
+
+		const newNumbers = [...numbers].map((number) => {
+			if (number.index !== i16 && number.index !== tile.index) return number;
+			else if (number.value === 16) return { value: 16, index: tile.index };
+
+			return { value: tile.value, index: i16 };
+		});
+		setNumbers(newNumbers);
+	};
 
 	return (
 		<div className="Game">
 			<div className="board">
 				<Overlay />
-				{numbers.map((x, i) => (
-					<Tile key={i} number={x} />
-				))}
+				{numbers.map((x, i) => {
+					return <Tile key={i} tileNumber={x} moveTile={moveTile} />;
+				})}
 			</div>
 		</div>
 	);
