c6ddd65ee5574b0af167e3805361869b77a0cbaa
AdamGiszter <a.m.giszter@gmail.com>
2023-11-24 17:47:01 +0100

    timer added

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -5,6 +5,7 @@ import Winner from "../winner/Winner";
 import shuffle from "../../Utils";
 import "./Board.css";
 import { useState, useEffect } from "react";
+import Timer from "../timer/Timer";
 
 interface tileData {
 	value: number;
@@ -15,6 +16,15 @@ export const Board = () => {
 	const [numbers, setNumbers] = useState<tileData[]>([]);
 	const [animating, setAnimating] = useState(false);
 
+	const [isRunning, setIsRunning] = useState(false);
+
+	useEffect(() => {
+		if (numbers.every((n) => n.value === n.index + 1)) setIsRunning(false);
+	}, [numbers]);
+
+	const startTimer = () => {
+		if (!isRunning) setIsRunning(!isRunning);
+	};
 	const moveTile = (tile: tileData) => {
 		const i16 = numbers.find((n) => n.value === 16)!.index;
 		if (![i16 - 1, i16 + 1, i16 - 4, i16 + 4].includes(tile.index) || animating)
@@ -28,8 +38,8 @@ export const Board = () => {
 		setAnimating(true);
 		setNumbers(newNumbers);
 		setTimeout(() => setAnimating(false), 200);
+		startTimer();
 	};
-
 	const handleKeyDown = (e: any) => {
 		const i16 = numbers.find((n) => n.value === 16)!.index;
 		if (e.keyCode === 37 && !(i16 % 4 === 3))
@@ -42,8 +52,14 @@ export const Board = () => {
 			moveTile(numbers.find((n) => n.index === i16 - 4)!);
 	};
 
-	const reset = () => setNumbers(shuffle);
+	const [time, setTime] = useState(false);
 
+	const reset = () => {
+		setNumbers(shuffle);
+		setTime(true);
+		setTimeout(() => setTime(false), 200);
+		setIsRunning(false);
+	};
 	useEffect(() => {
 		document.addEventListener("keydown", handleKeyDown);
 		return () => {
@@ -55,6 +71,7 @@ export const Board = () => {
 
 	return (
 		<div className="Game">
+			<Timer startTimer={isRunning} reset={time} />
 			<div className="board">
 				<Overlay />
 				{numbers.map((x, i) => {
@@ -62,7 +79,6 @@ export const Board = () => {
 				})}
 				<Winner numbers={numbers} />
 			</div>
-
 			<NewGame onClick={reset} />
 		</div>
 	);
